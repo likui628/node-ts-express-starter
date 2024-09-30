3d1be6f34b5d5027cc135cdb056e8845f9195cbe
Li Kui <90845831+likui628@users.noreply.github.com>
2024-09-30 22:21:28 +0800

    Password hashing (#18)
    * Password hashing
    
    * error handling for bcrypt operations
    
    * error handling for bcrypt operations

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,12 +1,25 @@
 import { userService } from '.'
+import bcrypt from 'bcrypt'
+import { logger } from '../config/logger'
 
 export const loginUserWithEmailAndPassword = async (
   email: string,
   password: string,
 ) => {
   const user = await userService.getUserByEmail(email)
-  if (!user || user.password !== password) {
-    throw new Error('password is incorrect')
+  if (!user) {
+    throw new Error('User is not exist')
   }
+
+  try {
+    const match = await bcrypt.compare(password, user.password)
+    if (!match) {
+      throw new Error('Password is incorrect')
+    }
+  } catch (error) {
+    logger.error('Error during password comparison:', error)
+    throw new Error('An error occurred during authentication')
+  }
+
   return user
 }
