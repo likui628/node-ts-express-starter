0cce469d16b9faf92c8354e7f57913a1524f3ead
likui628 <90845831+likui628@users.noreply.github.com>
2024-10-01 14:03:29 +0800

    Check token expires duration

--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -4,5 +4,8 @@ export const getTokenExpiration = (
   val: number,
   duration: keyof Duration = 'days',
 ) => {
+  if (val <= 0) {
+    throw new Error('Duration value must be a positive number')
+  }
   return add(new Date(), { [duration]: val })
 }
