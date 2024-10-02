02c1808ce0b892730b6d530e0784b73a3df54e52
likui628 <90845831+likui628@users.noreply.github.com>
2024-10-02 14:34:45 +0800

    Update validate

--- src/middlewares/validate.ts
+++ src/middlewares/validate.ts
@@ -1,13 +1,23 @@
-import { ZodSchema } from 'zod'
+import { ZodError, ZodSchema } from 'zod'
 import { NextFunction, Request, Response } from 'express'
 import { errorResponse } from '../utils'
+import { logger } from '../config/logger'
 
 export function validate(schema: ZodSchema) {
   return (req: Request, res: Response, next: NextFunction) => {
     try {
-      schema.parse(req.body)
+      const validatedRequest = schema.parse({
+        body: req.body,
+        params: req.params,
+        query: req.query,
+      })
+      const { params, query, body } = validatedRequest
+      Object.assign(req, { params, query, body })
       next()
     } catch (err: unknown) {
+      if (err instanceof ZodError) {
+        logger.error(`Validation Errors: ${JSON.stringify(err.issues)}`)
+      }
       errorResponse(res, err, 400, 'Validation Error')
     }
   }
