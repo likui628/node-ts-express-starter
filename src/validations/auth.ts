f9998677ece21b5b23c22716708375d4f8797128
Li Kui <90845831+likui628@users.noreply.github.com>
2024-10-02 14:45:44 +0800

    Update validate middleware(#25)

--- src/validations/auth.ts
+++ src/validations/auth.ts
@@ -1,12 +1,16 @@
 import { z } from 'zod'
 
 export const userRegisterSchema = z.object({
-  email: z.string().email(),
-  password: z.string().min(6),
-  name: z.string(),
+  body: z.object({
+    email: z.string().email(),
+    password: z.string().min(6),
+    name: z.string(),
+  }),
 })
 
-export const userLoginSchema = userRegisterSchema.pick({
-  email: true,
-  password: true,
+export const userLoginSchema = z.object({
+  body: z.object({
+    email: z.string().email(),
+    password: z.string().min(6),
+  }),
 })
